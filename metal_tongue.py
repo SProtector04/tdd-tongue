ab893637e7653cbdefb9e7917eef641dda7ab01d
Adrián David Laínez Hernández <adriandavidlainezhernandez@gmail.com>
2026-04-16 16:19:28 -0600

    Primera implementación de Personaje y pruebas iniciales.

---
+++ metal_tongue.py
@@ -0,0 +1,16 @@
+class Personaje:
+    def __init__(self):
+        self.hp = 1000
+        self.nivel = 1
+        self.esta_vivo = True
+        
+    def atacar(self, objetivo, dano):
+        objetivo.hp -= dano
+        if objetivo.hp <= 0:
+            objetivo.hp = 0
+            objetivo.esta_vivo = False
+    def curar(self, objetivo, cura):
+        objetivo.hp += cura
+        if not objetivo.esta_vivo:
+            return  # Regla: Los muertos no se curan
+
---
+++ test_tongue.py
@@ -0,0 +1,38 @@
+from metal_tongue import Personaje
+
+def test_personaje_nace_con_estadisticas_correctas():
+    # Arrange (Preparar)
+    heroe = Personaje()
+    
+    # Assert (Verificar)
+    assert heroe.hp == 1000
+    assert heroe.nivel == 1
+    assert heroe.esta_vivo == True
+
+def test_personaje_recibe_dano():
+    heroe = Personaje()
+    enemigo = Personaje()
+    
+    # Act (Actuar)
+    enemigo.atacar(heroe, dano=200)
+    
+    # Assert
+    assert heroe.hp == 800
+    
+def test_personaje_muere_si_hp_llega_a_cero():
+    heroe = Personaje()
+    enemigo = Personaje()
+    
+    enemigo.atacar(heroe, dano=1500)
+    
+    assert heroe.hp == 0  # El HP no debe quedar en -500
+    assert heroe.esta_vivo == False
+
+def test_curar_personaje():
+    heroe = Personaje()
+    enemigo = Personaje()
+    
+    enemigo.atacar(heroe, dano=200)
+    heroe.curar(heroe, cura=100)
+    
+    assert heroe.hp == 900   #Debe tener 900
